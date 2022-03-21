193258de35692363bb60a370454bb8e9d81c1957
MattisWammes <mattiswammes@gmail.com>
2022-03-21 22:38:25 +0100

    Initial commit

---
+++ src/main/java/net/mattwamm/froststruck/Froststruck.java
@@ -0,0 +1,4 @@
+package net.mattwamm.froststruck;
+
+public class Froststruck {
+}
---
+++ src/main/java/net/mattwamm/froststruck/client/FroststruckClient.java
@@ -0,0 +1,5 @@
+package net.mattwamm.froststruck.client;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class FroststruckClient {
+}
